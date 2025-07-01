ab6df0e2d64ab153ed7212193591aeff99c7c71f
nabibaba90 <kolsuzpabg@gmail.com>
2025-07-01 10:58:11 +0300

    Update bot.py

--- sorgu/bot.py
+++ sorgu/bot.py
@@ -8,14 +8,15 @@ api_id = 24396967
 api_hash = '7ddf52c9da17c98550cb937552ebffb8'
 string_session = "1ApWapzMBuzs9njNDBHSqIOUiOdyn1y94Q3V-83VeoMC_uWXUo1K_ry2yiY7cGF2agjIp7nGZbTnsuaISM_KI3HIjN-bJX9HxfzoM7WS7JVz0zQ-R8-7m6HyClj00hGJetHYjUyF8DqqtsCy6k4J8rHphPJxnEPKhm5-9hoyxo8042AheQIhhen2Fx7dOUfY5vzsrXE8t-CefDdZbGqpGqQs0YBOMFFs2JYw37uAGkZdoTOEpolld64BBg5L_qPfTf3gn7Zyi_qOoK5zpV__m3KG2QPI9Pobxrlr76WqrPMsBfI4CormpxOAWo2WrVjLxomZfyv8s1vD_H_-6JjzAZ0eLrhcUhJc="
 
-# Yeni bir event loop (Flask ile çakışmaması için)
+# Yeni event loop oluştur
 loop = asyncio.new_event_loop()
 
-# Telegram Client tanımla
+# Telegram Client tanımla (loop bu loop)
 client = TelegramClient(StringSession(string_session), api_id, api_hash, loop=loop)
 
-# Thread içinde başlat
+# Thread içinde client başlat
 def basla():
+    asyncio.set_event_loop(loop)  # ← bunu eklemek şart!
     try:
         loop.run_until_complete(client.start())
         print("✅ Telegram Client başlatıldı.")
@@ -23,9 +24,10 @@ def basla():
     except Exception as e:
         print("❌ Telegram başlatılırken hata:", str(e))
 
+# Thread başlat
 threading.Thread(target=basla, daemon=True).start()
 
-# Komut gönderme fonksiyonu (panel backend burayı çağırır)
+# Komut gönderme fonksiyonu
 async def gonder_ve_bekle(komut):
     print("🔁 Komut gönderiliyor:", komut)
     cevaplar = []
